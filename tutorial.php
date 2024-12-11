bd26f4983df97d3c3f26a10e73852b3d245f0245
Jaeseong <easeon78@gmail.com>
2024-12-11 16:48:29 +0900

    HOME, Intro, Install

---
+++ tutorial.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// Home
+// echo "My first PHP script";
+
+// Install
+// $txt = "PHP";
+// echo "I love $txt!";
+echo phpversion();
+
+// Syntax
+?>
+
+</body>
+</html>
