025935a81bf432edc98ae92bb191b2901db8b19c
Jaeseong <easeon78@gmail.com>
2024-12-17 08:27:07 +0900

    Syntax

--- tutorial.php
+++ tutorial.php
@@ -3,15 +3,27 @@
 <body>
 
 <?php
-// Home
+/* Home */
 // echo "My first PHP script";
 
-// Install
+/* Install */
 // $txt = "PHP";
 // echo "I love $txt!";
-echo phpversion();
+// echo phpversion();
 
-// Syntax
+/* Syntax */
+// echo "Hello World!<br>";
+// // Not case sensitive for keywords
+// Echo "Hello World!<br>";
+// echo "Hello World!<br>";
+// EcHo "Hello World!<br>";
+// // Case sensitive for variable
+// $color = "red";
+// echo "My car is " . $color . "<br>";
+// echo "My house is " . $COLOR . "<br>";
+// echo "My boat is " . $coLOR . "<br>";
+
+/* Comments */
 ?>
 
 </body>
